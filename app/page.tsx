ff18be94b6aa036372e8a45b7abe5be6536dad96
vowval <jagannath.g@tringapps.com>
2024-02-14 18:27:43 +0530

    Next Js Sanity data

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,29 @@
 import Image from "next/image";
+import { client } from "../utils/sanity/client"
+
+
+type Post = {
+  _id: string
+  title?: string
+  slug?: {
+    current: string
+  }
+}
+
+export async function PostIndex() {
+  const posts = await client.fetch<Post[]>(`*[_type == "news"]`)
+
+  return (
+    <ul>
+      {posts.map((post) => (
+        <li key={post._id}>
+          <a href={post?.slug.current}>{post?.title}</a>
+        </li>
+      ))}
+    </ul>
+  )
+}
+
 
 export default function Home() {
   return (
